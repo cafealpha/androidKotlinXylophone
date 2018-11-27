b9876d55ee89ce1ac7e969f0f1a3a4334325b6f6
cafealpha <cyanine@naver.com>
2018-11-27 22:05:50 +0900

    초기설정

---
+++ app/src/main/java/com/example/hi/xylophone/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hi.xylophone
+
+import android.content.pm.ActivityInfo
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
